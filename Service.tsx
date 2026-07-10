864efc8a3e09975ffa078819acd134118016f9af
beeradllc <a.monico@yahoo.com>
2026-07-10 13:01:12 -0500

    Add Services component with service list

---
+++ Service.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Services = () => (
+  <div style={{ backgroundColor: 'black', color: 'gold', minHeight: '100vh', padding: '2rem' }}>
+    <h1>Our Services</h1>
+    <ul>
+      <li>Diagnostics</li>
+      <li>Electrical Systems</li>
+      <li>Hydraulics</li>
+      <li>HVAC</li>
+      <li>General Repair</li>
+    </ul>
+  </div>
+);
+
+export default Services;
